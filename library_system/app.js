bf6ee06265cbf4eb407c52bc0e43bd4421571a7b
trgw_ubig_v_13 <tregawa26@gmail.com>
2020-11-04 01:21:12 +0000

    displayed internal data of mysql

--- library_system/app.js
+++ library_system/app.js
@@ -29,7 +29,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/all', allRouter)
+app.use('/all', allRouter);
+//app.use('/db', dbRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- library_system/settings.js
+++ library_system/settings.js
@@ -1,6 +1,6 @@
 exports.server_port = 1337;
-exports.server_host = '192.168.33.11';
+exports.server_host = '192.168.33.13';
 exports.title = '図書検索';
-exports.top = "http://192.168.33.11:1337/";
+exports.top = "http://192.168.33.13:1337/";
 exports.all = this.top + "all/";
 exports.libraryName = 'ubiquitous library';
