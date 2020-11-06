00e1da1bf873bf5b91a42bbc821e79e5fac4d670
trgw_ubig_v_13 <tregawa26@gmail.com>
2020-11-06 09:02:31 +0000

    検索機能で図書検索

--- library_system/routes/result.js
+++ library_system/routes/result.js
@@ -17,47 +17,26 @@ let db = require('../db');
 //   });
 // });
 
-// 検索ボタンを押したら get で文字列と完全一致する要素（title, author, genre, publisher）をすべて表示
-router.get('/', function(req, res, next) {
-  const search_string = req.body.search_string;
-  db.connection.query(
-    ('SELECT * FROM books WHERE `title` = "?" OR `author` = "?" OR `genre` = "?" OR `publisher` = "?";',
-    search_string, search_string, search_string, search_string
-    ), function (err, rows, fields) {
-      if (err) {
-        throw err;
-      } else {
-        res.render('result', {
-          title: settings.title,
-          index: settings.index,
-          all: settings.all,
-          libraryName: settings.libraryName,
-          content: rows
-        });
-      }
+router.post('/', function(req, res, next) {
+  const search_string = req.body.search_input;
+  console.log(req.body.search_input);
+  db.connection.query('SELECT * FROM books WHERE title = ?', [search_string], function (err, rows, fields) {
+    if (err) {
+      throw err;
+    } else {
+
+      console.log(rows);
+
+      res.render('result', {
+        title: settings.title,
+        index: settings.index,
+        all: settings.all,
+        libraryName: settings.libraryName,
+        content: rows
+      });
+    }
   });
 });
 
-
-// postで
-// router.post('/', function(req, res, next) {
-//   const search_string = req.body.search_string;
-//   db.connection.query(
-//     ('SELECT * FROM books WHERE `title` = "?" OR `author` = "?" OR `genre` = "?" OR `publisher` = "?";',
-//     search_string, search_string, search_string, search_string
-//     ), function (err, rows, fields) {
-//       if (err) {
-//         throw err;
-//       } else {
-//         res.render('result', {
-//           title: settings.title,
-//           index: settings.index,
-//           all: settings.all,
-//           libraryName: settings.libraryName,
-//           content: rows
-//         });
-//       }
-//   });
-// });
 
 module.exports = router;
