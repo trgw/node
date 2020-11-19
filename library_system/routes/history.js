a54296fae3185087d8bf57c529c42673f9710f81
trgw_ubig_v_13 <tregawa26@gmail.com>
2020-11-19 09:13:47 +0000

    index page に画像

--- library_system/routes/history.js
+++ library_system/routes/history.js
@@ -25,4 +25,28 @@ router.get('/', function(req, res, next) {
   });
 });
 
+/* book のデータから */
+router.post('/', function(req, res, next) {
+
+  let book_id = parseInt(req.body.book_name);
+
+  db.connection.query('SELECT * FROM history WHERE book_id = ?', [book_id], function(err, rows, fields) {
+    if (err) {
+      throw err;
+    } else {
+      res.render('history', {
+        title: settings.title,
+        login: settings.login,
+        index: settings.index,
+        books: settings.books,
+        users: settings.users,
+        history: settings.history,
+        result: settings.result,
+        libraryName: settings.libraryName,
+        content: rows
+      })
+    }
+  })
+})
+
 module.exports = router;
